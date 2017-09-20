d7b67389ebbcc1dad2814f0356634ba8e1fe357c
angelicaperez37 <angelica.perez37@gmail.com>
2017-09-20 12:51:26 -0400

    Initial commit. Created basic, empty, mini crossword board.

---
+++ src/index.js
@@ -0,0 +1,105 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import './css/index.css';
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+class Board extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            squares: Array(25).fill(null),
+            solution: [
+                'A', 'T', 'L', 'A', 'S',
+                'C', 'R', 'U', 'S', 'H',
+                'H', 'I', 'P', 'P', 'O',
+                'I', 'C', 'I', 'E', 'R',
+                'P', 'E', 'N', 'N', 'E'
+            ],
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+    }
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(5)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                    {this.renderSquare(9)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(10)}
+                    {this.renderSquare(11)}
+                    {this.renderSquare(12)}
+                    {this.renderSquare(13)}
+                    {this.renderSquare(14)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(15)}
+                    {this.renderSquare(16)}
+                    {this.renderSquare(17)}
+                    {this.renderSquare(18)}
+                    {this.renderSquare(19)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(20)}
+                    {this.renderSquare(21)}
+                    {this.renderSquare(22)}
+                    {this.renderSquare(23)}
+                    {this.renderSquare(24)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game/>,
+    document.getElementById('root')
+);
---
+++ src/js/Solution.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export class Solution extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            squares: null
+        }
+    }
+}
